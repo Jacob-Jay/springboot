79b5cbcc8b0713ee595e25b9975eabfb175725a9
jiangqing <819657451@qq.com>
2019-09-03 20:33:43 +0800

    初始化

---
+++ spbaction/src/main/java/com/jq/spbAction/SpbactionApplication.java
@@ -0,0 +1,13 @@
+package com.jq.spbAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbactionApplication.class, args);
+	}
+
+}
---
+++ spbaction/src/test/java/com/jq/spbAction/SpbactionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jq.spbAction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpbactionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
